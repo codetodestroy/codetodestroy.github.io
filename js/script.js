96b978c715bde787b21295ab722cfcf7194cea50
Caio Douglas <caiodqribeiro@gmail.com>
2023-10-14 10:47:22 -0300

    menu mobile

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-const caroussel = document.querySelector(".categories-list");
+const carousselCategories = document.querySelector(".categories-list");
 const category = document.querySelector('.category');
 const buttons = document.querySelectorAll(".btn-caroussel");
 
 buttons[0].addEventListener('click', function() {
     const categoryWidth = category.clientWidth;
-    caroussel.scrollLeft -= categoryWidth;
+    carousselCategories.scrollLeft -= categoryWidth;
 });
 
 buttons[1].addEventListener('click', function(e) {
     const categoryWidth = category.clientWidth;
-    caroussel.scrollLeft += categoryWidth;
+    carousselCategories.scrollLeft += categoryWidth;
+});
+
+const openMenuMobile = document.querySelector('.btn-burger');
+const closeMenuMobile = document.querySelector('.close-header-menu');
+const modelMenuMobile = document.querySelector('.header-nav-menu-modal-visitant');
+
+openMenuMobile.addEventListener('click', () => {
+    modelMenuMobile.style.display = 'flex';
+});
+
+closeMenuMobile.addEventListener('click', () => {
+    alert('fechou!');
 });
