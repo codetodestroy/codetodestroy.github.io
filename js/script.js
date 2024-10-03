a1de65c39682f709d0871ed5a8f3df4a0c3d64af
Caio Douglas <caiodqribeiro@gmail.com>
2024-10-02 21:31:11 -0300

    Finishing JS

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ const btnAddItem = document.querySelector('.btnAddItem');
 
 const itemsList = document.querySelector('#list-items');
 
-const item = document.querySelector('li.hide');
+let idAndNameNewItem = 1;
 
-const addedItems = itemsList.querySelectorAll('.item');
-
-let idAndNameNewItem = 0;
+let btnRemove;
 
 btnAddItem.addEventListener('click', () => {
 
@@ -16,20 +14,49 @@ btnAddItem.addEventListener('click', () => {
         inItem.focus();
     } else {
         let messageItem = inItem.value;
-        let newItem = item.cloneNode(true);
 
-        newItem.classList.add('item');
-        newItem.classList.remove('hide');
+        // Creating a new li element
+        let li = document.createElement('li');
+        // Adding item class in the element
+        li.classList.add('item');
 
-        newItem.childNodes[1].id = 'item-' + idAndNameNewItem;
-        newItem.childNodes[1].name = 'item-' + idAndNameNewItem;
-        newItem.childNodes[1].value = messageItem;
+        // Creating a new input element
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = 'item-' + idAndNameNewItem;
+        input.name = 'item-' + idAndNameNewItem;
+        input.value = messageItem;
 
-        newItem.childNodes[3].htmlFor = 'item-' + idAndNameNewItem;
-        newItem.childNodes[3].textContent = messageItem;
+        // Creating a new label element
+        let label = document.createElement('label');
+        label.htmlFor = 'item-' + idAndNameNewItem;
+        label.innerText = messageItem;
+
+        // Creating a new button element
+        let button = document.createElement('button');
+        button.classList.add('btnBin');
+
+        // Adding children in parent
+        li.append(input, label, button);
 
         idAndNameNewItem++;
 
-        itemsList.appendChild(newItem);
+        itemsList.appendChild(li);
+
+        inItem.value = '';
     }
+
+    btnRemove = document.querySelectorAll('#list-items li button');
+
+    btnRemove.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+
+            let deleteItemConfirm = confirm('Tem certeza que deseja deletar o item?');
+
+            if(deleteItemConfirm) {
+                btn.parentElement.remove();
+            }
+
+        });
+    });
 });
